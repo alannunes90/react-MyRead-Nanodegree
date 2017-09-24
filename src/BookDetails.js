f06f88ff41c79dc59377362272a818a981799d72
alannunes90 <alannunes90@gmail.com>
2017-09-24 20:22:00 -0300

    feat: Replace a component with a simple function
    Suggestion:
    
    Components that do not need state can be created as a simple function, making it easier to read, test and maintain them, since they only keep the part that matters from the code, removing what exists only by syntax.
    
    Below is an example of how these components are written:
    
    const ComponentName = (props) => (
      <div>{props.someProp}</div>
    )

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,34 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class BookDetails extends React.Component {
-    static propTypes = {
-        book: PropTypes.object.isRequired
-    }
-    render() {
-        const { book } = this.props
-    return (
-        <div>
-            <div className='book-details-list'>
-                <div key={book.id} className='book-details-list-item'>
-                    {book.imageLinks !== undefined && (
-                            <div className="book-cover" style={{ width: 128, height: 193, 
-                                backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
-                            </div>
-
-                        )}
-                    <div className='book-details'>
-                        <p className="book-details-text">Title: {book.title}</p>
-                        <p className="book-details-text">Authors: {book.authors}</p>
-                        <p className="book-details-text">Categories: {book.categories}</p>
-                        <p className="book-details-text">Description: {book.description}</p>
-                        <p className="book-details-text">Page Count: {book.pageCount}</p>
-                        <p className="book-details-text">Published Date: {book.publishedDate}</p>
+const BookDetails = (props) => (
+    <div className='book-details-list'>
+        <div key={props.book.id} className='book-details-list-item'>
+            {props.book.imageLinks !== undefined && (
+                    <div className="book-cover" style={{ width: 128, height: 193, 
+                        backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}>
                     </div>
-                </div>
+
+                )}
+            <div className='book-details'>
+                <p className="book-details-text">Title: {props.book.title}</p>
+                <p className="book-details-text">Authors: {props.book.authors}</p>
+                <p className="book-details-text">Categories: {props.book.categories}</p>
+                <p className="book-details-text">Description: {props.book.description}</p>
+                <p className="book-details-text">Page Count: {props.book.pageCount}</p>
+                <p className="book-details-text">Published Date: {props.book.publishedDate}</p>
             </div>
         </div>
-    )}
-}
+    </div>
+)
+
+BookDetails.propTypes = {
+    book: PropTypes.object.isRequired
+};
 
 export default BookDetails
